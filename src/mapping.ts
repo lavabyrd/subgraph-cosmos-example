da6a64261e9bbcba7da5f6b1a5e780d444d0b514
Patrycja Zawadka <patrycja@figment.io>
2021-12-23 15:39:26 +0100

    save tx result

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,52 +1,101 @@
-import { tendermint } from "@graphprotocol/graph-ts/chain/tendermint";
-import { BigInt, log } from "@graphprotocol/graph-ts";
-import { Bytes } from "@graphprotocol/graph-ts/common/collections";
-import { json } from "@graphprotocol/graph-ts/common/json";
-import { Value, ValueKind, ValuePayload } from "@graphprotocol/graph-ts/common/value";
+import { BigInt, log, tendermint } from "@graphprotocol/graph-ts";
 import {
   Block,
-  Evidence,
+  BlockID,
+  Data,
   Header,
-
   ResponseDeliverTx,
-  Reward
+  Reward,
+  TxResult
 } from "../generated/schema";
 
 export function handleBlock(el: tendermint.EventList): void {
-  const blockHash = el.newblock.block_id.hash.toHex();
-
-  const h = el.newblock.block.header;
-  const header = new Header(blockHash);
-  header.version = BigInt.fromString(h.version.app.toString()).toString() + BigInt.fromString(h.version.block.toString()).toString();
-  header.chain_id = h.chain_id;
-  header.height =  BigInt.fromString(h.height.toString());
-  header.time = new Date(h.time.seconds).toISOString();
-  header.save();
-
-  
   const b = el.newblock.block;
-  const block = new Block(blockHash);
-  block.data = json.fromBytes(new Value(ValueKind.BYTES, changetype<u32>(b.data.txs)).toBytes()).toString();
-  block.save()
-
+  const h = el.newblock.block.header;
+  const blockHash = el.newblock.block_id.hash.toHex();
+  const height = BigInt.fromString(h.height.toString());
+ 
+  saveHeader(blockHash, el.newblock.block.header, height);
+  saveBlockID(blockHash, el.newblock.block_id)
+  saveData(blockHash, b.data)
+  saveBlock(blockHash)
 
   for (let index = 0; index < el.transaction.length; index++) {
     const txResult = el.transaction[index].TxResult;
-    const responseDeliverTx = new ResponseDeliverTx(
-      h.data_hash.toHexString() + index.toString()
-    );
+    const txID = h.data_hash.toHexString() + index.toString();
 
-    responseDeliverTx.code = new BigInt(txResult.result.code);
-    responseDeliverTx.data = txResult.tx;
-    responseDeliverTx.log = txResult.result.log;
-    responseDeliverTx.info = txResult.result.info;
-    responseDeliverTx.gas_wanted = BigInt.fromString(txResult.result.gas_wanted.toString());
-    responseDeliverTx.gas_used = BigInt.fromString(txResult.result.gas_used.toString());
-    responseDeliverTx.codespace = txResult.result.codespace;
-    responseDeliverTx.save();
-
-    // const transaction = new Transac
+    saveResponseDeliverTx(txID, txResult);
+    saveTxResult(txID, height, BigInt.fromI32(index), txResult)
   }
+}
+
+function saveHeader(id: string, h: tendermint.Header, height: BigInt): void {
+  const header = new Header(id);
+
+  header.version = BigInt.fromString(h.version.app.toString()).toString() + BigInt.fromString(h.version.block.toString()).toString();
+  header.chain_id = h.chain_id;
+  header.height =  height;
+  header.time = new Date(h.time.seconds).toISOString();
+  header.last_block_id = h.last_block_id.hash.toHex();
+  header.last_commit_hash = h.last_commit_hash;
+  header.data_hash = h.data_hash;
+  header.validators_hash = h.validators_hash;
+  header.next_validators_hash = h.next_validators_hash;
+  header.consensus_hash = h.consensus_hash;
+  header.app_hash = h.app_hash;
+  header.last_results_hash = h.last_results_hash;
+  header.evidence_hash = h.evidence_hash;
+  header.proposer_address = h.proposer_address.toHex();
+
+  header.save();
+}
+
+function saveBlockID(id: string, bID: tendermint.BlockID): void {
+  const blockID = new BlockID(id);
+
+  blockID.hash = bID.hash;
+
+  blockID.save();
+}
+
+function saveData(id: string, d: tendermint.Data): void {
+  const data = new Data(id);
+
+  data.txs = d.txs;
+
+  data.save();
+}
+
+function saveBlock(id: string): void {
+  const block = new Block(id);
+
+  block.data = id;
+  block.header = id;
+
+  block.save()
+}
+
+function saveResponseDeliverTx(id: string, txResult: tendermint.TxResult): void {
+  const responseDeliverTx = new ResponseDeliverTx(id);
+
+  responseDeliverTx.code = new BigInt(txResult.result.code);
+  responseDeliverTx.data = txResult.tx;
+  responseDeliverTx.log = txResult.result.log;
+  responseDeliverTx.info = txResult.result.info;
+  responseDeliverTx.gas_wanted = BigInt.fromString(txResult.result.gas_wanted.toString());
+  responseDeliverTx.gas_used = BigInt.fromString(txResult.result.gas_used.toString());
+  responseDeliverTx.codespace = txResult.result.codespace;
+
+  responseDeliverTx.save();
+}
+
+function saveTxResult(id: string, height: BigInt, index: BigInt, txRes: tendermint.TxResult): void {
+  const txResult = new TxResult(id);
+  txResult.height = height;
+  txResult.index = index;
+  txResult.tx = txRes.tx;
+  txResult.result = id;
+  txResult.save();
 }
 
 export function handleReward(eventData: tendermint.EventData): void {
@@ -61,18 +110,4 @@ export function handleReward(eventData: tendermint.EventData): void {
   reward.validator = validator;
 
   reward.save();
-}
-
-function bytesToString(bytes: Array<Bytes>): string {
-  // var result = "";
-  // for (var i = 0; i < bytes.length; i++) {
-  //   // let cast = parseFloat(bytes.at(i).toString()) as i32;
-  //   // let cast = 1;
-  //   result += String.fromCharCode(changetype<u32>(bytes.at(i)))
-  //   // result += String.fromCharCode(parseInt(bytes[i].toString(), 2));
-  // }
-  // return result;
-
-  return bytes.toString()
-  // return changetype<string>(bytes.to() as u32)
 }
