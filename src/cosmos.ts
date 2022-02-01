24313f50072e2527d0f473e37048d97ae9d01b1f
Patrycja Zawadka <patrycja@figment.io>
2022-02-01 05:58:39 +0100

    Clean up code

--- src/cosmos.ts
+++ src/cosmos.ts
@@ -1,225 +1,205 @@
-import { BigInt, Bytes, log } from "@graphprotocol/graph-ts";
-import { cosmos } from "@graphprotocol/graph-ts/chain/tendermint/cosmos";
-import { Protobuf } from "as-proto";
-import { AuthInfo, Coin, CompactBitArray, Fee, ModeInfo, Multi, SignerInfo, Single, Tx, TxBody,
+import { BigInt, Bytes, log, cosmos } from "@graphprotocol/graph-ts";
+import { AuthInfo, Coin, CompactBitArray, Fee, ModeInfo, MsgDelegate, MsgWithdrawDelegatorReward, Multi, PubKey, SignerInfo, Single, Tip, Tx, TxBody,
 } from "../generated/schema";
 
 export function decodeTxs(id: string, txs: Array<Bytes>): void {
-    const len = txs.length;
-    log.info("txs len: {}", [len.toString()]);
-
-    for (let i = 0; i < len; i++) {
-
-        // log.info("bytes: {} \n\n hex bytes: {}", [txs[i].toString(), txs[i].toHexString()])
-
-        let tx = decodeTx(txs[i]);
-        saveTx(`${id}-${i}`, tx);
-        // let typeURL: string = any.type_url as string;
-
-        // log.info("auth_info fee: ", [tx.auth_info.fee.amount.toString()])
-
-        // log.info("tx signatures len: {} \n", [tx.signatures.length.toString()]);
-
-        // let tx = decodeTxResponse(txs[i]);
-        // let txRaw = decodeTxBody(txs[i]);
-
-
-        // let txAny = decodeTx(any.value as Uint8Array);
-        // let txBody = decodeTxBody(txRaw.body_bytes as Uint8Array);
-
-        // let body: cosmos.v1.TxBody = tx.body as cosmos.v1.TxBody;
-        // let messages: Array<cosmos.v1.Any> = body.messages as Array<cosmos.v1.Any>;
-        // let memo: string = body.memo as string; // fee in memo??
-        // let timeout_height: u64 = body.timeout_height as u64;
-        // // let extension_options: Array<cosmos.v1.Any>;
-        // // let non_critical_extension_options: Array<cosmos.v1.Any>;
-
-
-        // // let authInfo: cosmos.v1.AuthInfo = tx.auth_info as cosmos.v1.AuthInfo;
-        // // let signatures: Array<Uint8Array> = tx.signatures as Array<Uint8Array>;
-
-        // // log.info("tx any type_url: {} \nmemo: {} \n timeout_height: {} \n", [typeURL, memo, timeout_height.toString()]);
-
-        // log.info("msgs len: {}", [messages.length.toString()]);
-
-        // for (let j=0; j< messages.length; j++) {
-        //     // let message: cosmos.v1.Any = messages[i];
-        //     // let msg = decodeAny(messages[i]);
-        //     let msgTypeURL: string = messages[j].type_url as string;
-        //     log.info("msg {} any type_url: {}", [j.toString(), msgTypeURL]);
-        // }
-
-
-        // let value: Uint8Array = any.value as Uint8Array;
-    }
+    txs.forEach((tx, i) => {
+        saveTx(`${id}-${i}`, cosmos.v1.decodeTx(tx));
+    });
 }
 
-// export function decodeTx(tx: Bytes): void {
-//     let any = decodeAny(tx);
-//     let typeURL: string = any.type_url as string;
-
-//     log.info("tx any type_url: {} \n", [typeURL]);
-// }
-
 function saveTx(id: string, tx: cosmos.v1.Tx): void{
-    saveAuthInfo(id, tx.auth_info as cosmos.v1.AuthInfo);
-    saveBody(id, tx.body as cosmos.v1.TxBody);
-
     const cTx = new Tx(id);
-    cTx.auth_info = id;
-    cTx.body = id;
+    cTx.auth_info = saveAuthInfo(id, tx.auth_info as cosmos.v1.AuthInfo);
+    cTx.body = saveBody(id, tx.body as cosmos.v1.TxBody);
     cTx.save();
 }
 
-function saveAuthInfo(id: string, ai: cosmos.v1.AuthInfo): void{
-    saveFee(id, ai.fee as cosmos.v1.Fee);
-
+function saveAuthInfo(id: string, ai: cosmos.v1.AuthInfo): string {
     const authInfo = new AuthInfo(id);
-    authInfo.fee = id;
+    authInfo.fee = saveFee(`${id}-fee`, ai.fee as cosmos.v1.Fee);;
     authInfo.signer_infos = saveSignerInfos(id, ai.signer_infos);
-    // authInfo.tip
+    authInfo.tip = saveTip(`${id}-tip`, ai.tip as cosmos.v1.Tip);;
     authInfo.save()
+    return id;
 }
 
-function saveFee(id: string, f: cosmos.v1.Fee): void {
+function saveFee(id: string, f: cosmos.v1.Fee): string {
     const fee = new Fee(id);
     fee.amount = saveCoins(id, f.amount);
     fee.gas_limit = BigInt.fromString(f.gas_limit.toString());
     fee.payer = f.payer;
     fee.granter = f.granter;
     fee.save();
+    return id;
 }
 
-function saveCoins(id: string, coins: Array<cosmos.v1.Coin>): Array<string> | null {
+function saveTip(id: string, t: cosmos.v1.Tip): string {
+    const tip = new Tip(id);
+    tip.amount = saveCoins(id, t.amount);
+    tip.tipper = t.tipper;
+    tip.save();
+    return id;
+}
+
+function saveCoins(id: string, coins: Array<cosmos.v1.Coin>): Array<string> {
     const len = coins.length;
     let coinIDs = new Array<string>(len);
     for (let i = 0; i < len; i++) {
-        saveCoin(`${id}-${i}`, coins[i])
+        coinIDs[i] = saveCoin(`${id}-${i}`, coins[i]);
     }
     return coinIDs;
 }
 
-function saveCoin(id: string, c: cosmos.v1.Coin): void{
+function saveCoin(id: string, c: cosmos.v1.Coin): string {
     const coin = new Coin(id);
     coin.amount = c.amount;
     coin.denom = c.denom;
     coin.save();
+    return id;
 }
 
-function saveSignerInfos(id: string, sis: Array<cosmos.v1.SignerInfo>): Array<string> | null {
-    const len = sis.length;
-    let signerInfoIDs = new Array<string>(len);
-    for (let i = 0; i < len; i++) {
-        saveSignerInfo(`${id}-${i}`, sis[i])
-    }
+function saveSignerInfos(id: string, sis: Array<cosmos.v1.SignerInfo>): Array<string> {
+    let signerInfoIDs = new Array<string>(sis.length);
+    sis.forEach((si, i) => {
+        signerInfoIDs[i] = saveSignerInfo(`${id}-${i}`, si);
+    });
     return signerInfoIDs;
 }
 
-function saveSignerInfo(id: string, si: cosmos.v1.SignerInfo): void {
-    savePublicKey(id, si.public_key as cosmos.v1.Any);
-    saveModeInfo(id, si.mode_info as cosmos.v1.ModeInfo);
-
+function saveSignerInfo(id: string, si: cosmos.v1.SignerInfo): string {
     const signerInfo = new SignerInfo(id);
-    signerInfo.public_key = id;
-    signerInfo.mode_info = id;
+    signerInfo.public_key = savePublicKey(id, si.public_key as cosmos.v1.Any);
+    signerInfo.mode_info = saveModeInfo(id, si.mode_info as cosmos.v1.ModeInfo);
     signerInfo.sequence = BigInt.fromString(si.sequence.toString());
     signerInfo.save();
+    return id;
 }
 
-function savePublicKey(id: string, pk: cosmos.v1.Any): void{
+function savePublicKey(id: string, pk: cosmos.v1.Any): string {
+    let pkURL = pk.type_url as string;
     let pv = pk.value as Uint8Array;
-    log.info("public key: {} value: {}", [pk.type_url as string, pv.toString()])
+
+    if (pkURL == "/cosmos.crypto.secp256k1.PubKey") {
+        return saveSecp256k1PublicKey(id, cosmos.v1.decodePubKey(pv));
+    }
+
+    log.info("public key: {} value: {}", [pkURL, pv.toString()])
+    log.error("Unknown public key type {}", [pkURL]);
 }
 
-function saveModeInfo(id: string, mi: cosmos.v1.ModeInfo): void {
+function saveSecp256k1PublicKey(id: string, pk: cosmos.v1.PubKey): string {
+    const publicKey = new PubKey(id);
+    publicKey.key = Bytes.fromUint8Array(pk.key as Uint8Array);
+    publicKey.save()
+    return id;
+}
+
+function saveModeInfos(id: string, mis: Array<cosmos.v1.ModeInfo>): Array<string> {
+    let modeInfoIDs = new Array<string>(mis.length);
+    mis.forEach((mi, i) => {
+        modeInfoIDs[i] = saveModeInfo(`${id}-${i}`, mi);
+    });
+    return modeInfoIDs;
+}
+
+function saveModeInfo(id: string, mi: cosmos.v1.ModeInfo): string {
     const modeInfo = new ModeInfo(id);
     if (mi.single !== null) {
-        saveSingle(id, mi.single as cosmos.v1.Single);
-        modeInfo.single = id;
+        modeInfo.single = saveSingle(id, mi.single as cosmos.v1.Single);
     }
     if (mi.multi !== null) {
-        saveMulti(id, mi.multi as cosmos.v1.Multi);
-        modeInfo.multi = id;
+        modeInfo.multi = saveMulti(id, mi.multi as cosmos.v1.Multi);
     }
-    
-    modeInfo.save()
+    modeInfo.save();
+    return id;
 }
 
-function saveSingle(id: string, s: cosmos.v1.Single): void {
+function saveSingle(id: string, s: cosmos.v1.Single): string {
     const single = new Single(id);
-    single.mode = s.mode.toString();
+    single.mode = getSignMode(s.mode);
     single.save();
+    return id;
 }
 
-function saveMulti(id: string, m: cosmos.v1.Multi): void {
-    saveCompactBitArray(id, m.bitarray as cosmos.v1.CompactBitArray);
-    
+function saveMulti(id: string, m: cosmos.v1.Multi): string {
     const multi = new Multi(id);
-    multi.bitarray = id;
+    multi.bitarray = saveCompactBitArray(id, m.bitarray as cosmos.v1.CompactBitArray);
+    multi.mode_infos = saveModeInfos(id, m.mode_infos);
     multi.save();
+    return id;
 }
 
-function saveCompactBitArray(id: string, c: cosmos.v1.CompactBitArray): void{
+function getSignMode(mi: cosmos.v1.SignMode): string {
+    switch (mi) {
+        case cosmos.v1.SignMode.SIGN_MODE_UNSPECIFIED:
+            return "SIGN_MODE_UNSPECIFIED";
+        case cosmos.v1.SignMode.SIGN_MODE_DIRECT:
+            return "SIGN_MODE_DIRECT";
+        case cosmos.v1.SignMode.SIGN_MODE_TEXTUAL:
+            return "SIGN_MODE_TEXTUAL";
+        case cosmos.v1.SignMode.SIGN_MODE_DIRECT_AUX:
+            return "SIGN_MODE_DIRECT_AUX";
+        case cosmos.v1.SignMode.SIGN_MODE_LEGACY_AMINO_JSON:
+            return "SIGN_MODE_LEGACY_AMINO_JSON";
+        default:
+            log.error("Unknown mode info {}", [mi.toString()]);
+            return "unknown";
+    }
+}
+
+function saveCompactBitArray(id: string, c: cosmos.v1.CompactBitArray): string {
     const compactBitArray = new CompactBitArray(id);
     compactBitArray.extra_bits_stored = BigInt.fromString(c.extra_bits_stored.toString());
     compactBitArray.elems = Bytes.fromUint8Array(c.elems as Uint8Array);
     compactBitArray.save();
+    return id;
 }
 
-function saveBody(id: string, b: cosmos.v1.TxBody): void{
+function saveBody(id: string, b: cosmos.v1.TxBody): string {
     const txBody = new TxBody(id);
     txBody.messages = saveMessages(id, b.messages);
     txBody.memo = b.memo;
     txBody.timeout_height = BigInt.fromString(b.timeout_height.toString());
-    txBody.extension_options = saveExtensionOptions(`${id}-extension_options`, b.extension_options);
-    txBody.non_critical_extension_options = saveExtensionOptions(`${id}-non_critical_extension_options`, b.non_critical_extension_options);
     txBody.save();
+    return id;
 }
 
 function saveMessages(id: string, msgs: Array<cosmos.v1.Any>): Array<string>{
-    const len = msgs.length;
-    let messegeIDs = new Array<string>(len);
-    for (let i = 0; i < len; i++) {
-        saveMessege(`${id}-${i}`, msgs[i])
+    let messageIDs = new Array<string>(msgs.length);
+    msgs.forEach((msg, i) => {
+        messageIDs[i] = saveMessage(`${id}-${i}`, msg);
+    });
+    return messageIDs;
+}
+
+function saveMessage(id: string, msg: cosmos.v1.Any): string {
+    let msgType = msg.type_url as string;
+    let value = msg.value as Uint8Array;
+
+    if (msgType == "/cosmos.distribution.v1beta1.MsgWithdrawDelegatorReward") {
+        saveMsgWithdrawDelegatorReward(id, cosmos.v1.decodeMsgWithdrawDelegatorReward(value));
+        return;
+    } else if (msgType == "/cosmos.staking.v1beta1.MsgDelegate") {
+        saveMsgDelegate(id, cosmos.v1.decodeMsgDelegate(value));
+        return;
     }
-    return messegeIDs;
+
+    log.info("unknown msg type: {}", [msgType])
+    return id;
 }
 
-function saveMessege(id: string, msg: cosmos.v1.Any): void {
-    let v = msg.value as Uint8Array;
-    log.info("msg type: {} value {} ", [msg.type_url as string, v.toString()])
+function saveMsgDelegate(id: string, m: cosmos.v1.MsgDelegate): void {
+    const msg = new MsgDelegate(id);
+    msg.delegator_address = m.delegator_address;
+    msg.validator_address = m.validator_address;
+    msg.amount = saveCoin(id, m.amount as cosmos.v1.Coin);
+    msg.save();
 }
 
-function saveExtensionOptions(id: string, eops: Array<cosmos.v1.Any>): Array<string> {
-    const len = eops.length;
-    let extensionOptionIDs = new Array<string>(len);
-    for (let i = 0; i < len; i++) {
-        saveExtensionOption(`${id}-${i}`, eops[i])
-    }
-    return extensionOptionIDs;
-}
-
-function saveExtensionOption(id: string, eop: cosmos.v1.Any): void {
-    let v = eop.value as Uint8Array;
-    log.info("eop type: {} value {} ", [eop.type_url as string, v.toString()])
-}
-
-function decodeAny(data: Uint8Array): cosmos.v1.Any {
-    return Protobuf.decode<cosmos.v1.Any>(data, cosmos.v1.Any.decode);
-}
-
-function decodeTx(tx: Uint8Array): cosmos.v1.Tx {
-    return Protobuf.decode<cosmos.v1.Tx>(tx, cosmos.v1.Tx.decode);
-}
-
-function decodeTxResponse(tx: Uint8Array): cosmos.v1.TxResponse {
-    return Protobuf.decode<cosmos.v1.TxResponse>(tx, cosmos.v1.TxResponse.decode);
-}
-
-function decodeTxRaw(txRaw: Uint8Array): cosmos.v1.TxRaw {
-    return Protobuf.decode<cosmos.v1.TxRaw>(txRaw, cosmos.v1.TxRaw.decode);
-}
-
-function decodeTxBody(tx: Uint8Array): cosmos.v1.TxBody {
-    return Protobuf.decode<cosmos.v1.TxBody>(tx, cosmos.v1.TxBody.decode);
+function saveMsgWithdrawDelegatorReward(id: string, m: cosmos.v1.MsgWithdrawDelegatorReward): void {
+    const msg = new MsgWithdrawDelegatorReward(id);
+    msg.delegator_address = m.delegator_address;
+    msg.validator_address = m.validator_address;
+    msg.save()
 }
--- src/mapping.ts
+++ src/mapping.ts
@@ -9,6 +9,7 @@ import {
   DuplicateVoteEvidence,
   EventVote,
   Evidence,
+  EvidenceList,
   Header,
   LightBlock,
   LightClientAttackEvidence,
@@ -50,55 +51,47 @@ export function handleBlock(el: tendermint.EventList): void {
   log.info("BLOCK {} txs: {}", [height.toString(), txLen.toString()])
 }
 
-function saveBlockID(id: string, bID: tendermint.BlockID): void {
-  savePartSetHeader(id, bID.part_set_header);
-
+function saveBlockID(id: string, bID: tendermint.BlockID): string {
   const blockID = new BlockID(id);
   blockID.hash = bID.hash;
-  blockID.part_set_header = id;
+  blockID.part_set_header = savePartSetHeader(id, bID.part_set_header);;
   blockID.save();
+  return id;
 }
 
-function savePartSetHeader(id: string, psh: tendermint.PartSetHeader): void {
+function savePartSetHeader(id: string, psh: tendermint.PartSetHeader): string {
   const partSetHeader = new PartSetHeader(id);
   partSetHeader.total = BigInt.fromString(psh.total.toString());
   partSetHeader.hash = psh.hash;
   partSetHeader.save();
+  return id;
 }
 
 function saveBlock(id: string, b: tendermint.Block): void {
-  saveData(id, b.data);
-  saveHeader(id, b.header);
-  saveEvidenceList(id, b.evidence);
-  saveCommit(id, b.last_commit)
-
   const block = new Block(id);
-  block.data = id;
-  block.header = id;
-  block.evidence = id;
-  block.last_commit = id;
+  block.data = saveData(id, b.data);
+  block.header = saveHeader(id, b.header);
+  block.evidence = saveEvidenceList(id, b.evidence);
+  block.last_commit = saveCommit(id, b.last_commit);
   block.save()
 }
 
-function saveData(id: string, d: tendermint.Data): void {
+function saveData(id: string, d: tendermint.Data): string {
   decodeTxs(id, d.txs);
 
   const data = new Data(id);
   data.txs = d.txs;
   data.save();
+  return id;
 }
 
-function saveHeader(id: string, h: tendermint.Header): void {
+function saveHeader(id: string, h: tendermint.Header): string {
   const height = BigInt.fromString(h.height.toString());
-
-  saveTimestamp(id, h.time);
-  saveVersion(id, h.version);
-
   const header = new Header(id);
-  header.version = id;
+  header.version = saveVersion(id, h.version);
   header.chain_id = h.chain_id;
   header.height = height;
-  header.time = id;
+  header.time = saveTimestamp(id, h.time);
   header.last_block_id = h.last_block_id.hash.toHexString();
   header.last_commit_hash = h.last_commit_hash;
   header.data_hash = h.data_hash;
@@ -110,171 +103,154 @@ function saveHeader(id: string, h: tendermint.Header): void {
   header.evidence_hash = h.evidence_hash;
   header.proposer_address = h.proposer_address;
   header.save();
+  return id;
 }
 
-function saveVersion(id: string, v: tendermint.Consensus): void {
+function saveVersion(id: string, v: tendermint.Consensus): string {
   const version = new Consensus(id);
   version.block = BigInt.fromString(v.block.toString());
   version.app = BigInt.fromString(v.app.toString());
   version.save();
+  return id;
 }
 
-function saveTimestamp(id: string, ts: tendermint.Timestamp): void {
+function saveTimestamp(id: string, ts: tendermint.Timestamp): string {
   const timestamp = new Timestamp(id);
   timestamp.seconds = BigInt.fromString(ts.seconds.toString());
   timestamp.nanos = ts.nanos;
   timestamp.save();
+  return id;
 }
 
-function saveEvidenceList(id: string, el: tendermint.EvidenceList): Array<string> {
-  const len = el.evidence.length;
-  let evidenceIDs = new Array<string>(len);
-  for (let i = 0; i < len; i++) {
-    const evidence = el.evidence[i];
-    const evidenceID = `${id}-${i}`;
-    saveEvidence(evidenceID, evidence);
-    evidenceIDs[i] = evidenceID;
-  }
+function saveEvidenceList(id: string, el: tendermint.EvidenceList): string {
+  const evidenceList = new EvidenceList(id);
+  evidenceList.evidence = saveEvidences(id, el.evidence);
+  evidenceList.save();
+  return id;
+}
+
+
+function saveEvidences(id: string, evs: Array<tendermint.Evidence>): Array<string> {
+  let evidenceIDs = new Array<string>(evs.length);
+  evs.forEach((e, i) => {
+    evidenceIDs[i] = saveEvidence(`${id}-${i}`, e);
+  });
   return evidenceIDs;
 }
 
-function saveEvidence(id: string, e: tendermint.Evidence): void {
+function saveEvidence(id: string, e: tendermint.Evidence): string {
   const evidence = new Evidence(id);
   if (e.duplicate_vote_evidence !== null) {
-    saveDuplicateVoteEvidence(id, e.duplicate_vote_evidence);
-    evidence.duplicate_vote_evidence = id;
+    evidence.duplicate_vote_evidence = saveDuplicateVoteEvidence(id, e.duplicate_vote_evidence);
 
   } else if (e.light_client_attack_evidence !== null) {
-    saveLightClientAttackEvidence(id, e.light_client_attack_evidence);
-    evidence.light_client_attack_evidence = id;
+    evidence.light_client_attack_evidence = saveLightClientAttackEvidence(id, e.light_client_attack_evidence);
   }
   evidence.save();
+  return id;
 }
 
-function saveDuplicateVoteEvidence(id: string, e: tendermint.DuplicateVoteEvidence): void {
-  const voteAID = `${id}-voteA`;
-  const voteBID = `${id}-voteB`;
-  saveEventVote(voteAID, e.vote_a);
-  saveEventVote(voteBID, e.vote_a);
-  saveTimestamp(id, e.timestamp);
-
+function saveDuplicateVoteEvidence(id: string, e: tendermint.DuplicateVoteEvidence): string {
   const duplicateVoteEvidence = new DuplicateVoteEvidence(id);
-  duplicateVoteEvidence.vote_a = voteAID;
-  duplicateVoteEvidence.vote_b = voteBID;
+  duplicateVoteEvidence.vote_a = saveEventVote(`${id}-voteA`, e.vote_a);
+  duplicateVoteEvidence.vote_b = saveEventVote(`${id}-voteB`, e.vote_a);
   duplicateVoteEvidence.total_voting_power = BigInt.fromString(e.total_voting_power.toString());
   duplicateVoteEvidence.validator_power = BigInt.fromString(e.validator_power.toString());
-  duplicateVoteEvidence.timestamp = id;
+  duplicateVoteEvidence.timestamp = saveTimestamp(id, e.timestamp);
   duplicateVoteEvidence.save();
+  return id;
 }
 
-function saveEventVote(id: string, ev: tendermint.EventVote): void {
-  saveTimestamp(id, ev.timestamp);
-
+function saveEventVote(id: string, ev: tendermint.EventVote): string {
   const eventVote = new EventVote(id);
   eventVote.event_vote_type = ev.event_vote_type.toString();
   eventVote.height = BigInt.fromString(ev.height.toString());
   eventVote.round = ev.round;
   eventVote.block_id = ev.block_id.hash.toHexString();
-  eventVote.timestamp = id;
+  eventVote.timestamp = saveTimestamp(id, ev.timestamp);
   eventVote.validator_address = ev.validator_address;
   eventVote.validator_index = ev.validator_index;
   eventVote.signature = ev.signature;
   eventVote.save();
+  return id;
 }
 
-function saveLightClientAttackEvidence(id: string, e: tendermint.LightClientAttackEvidence): void {
-  saveLightBlock(id, e.conflicting_block);
-
+function saveLightClientAttackEvidence(id: string, e: tendermint.LightClientAttackEvidence): string {
   const lightClientAttackEvidence = new LightClientAttackEvidence(id);
-  lightClientAttackEvidence.conflicting_block = id;
+  lightClientAttackEvidence.conflicting_block = saveLightBlock(id, e.conflicting_block);
   lightClientAttackEvidence.save();
+  return id;
 }
 
-function saveLightBlock(id: string, lb: tendermint.LightBlock): void {
-  saveSignedHeader(id, lb.signed_header);
-  saveValidatorSet(id, lb.validator_set);
-
+function saveLightBlock(id: string, lb: tendermint.LightBlock): string {
   const lightBlock = new LightBlock(id);
-  lightBlock.signed_header = id;
-  lightBlock.validator_set = id;
+  lightBlock.signed_header = saveSignedHeader(id, lb.signed_header);
+  lightBlock.validator_set = saveValidatorSet(id, lb.validator_set);
   lightBlock.save();
+  return id;
 }
 
-function saveSignedHeader(id: string, sh: tendermint.SignedHeader): void {
-  saveHeader(id, sh.header);
-  saveCommit(id, sh.commit);
-
+function saveSignedHeader(id: string, sh: tendermint.SignedHeader): string {
   const signedHeader = new SignedHeader(id);
-  signedHeader.header = id;
-  signedHeader.commit = id;
+  signedHeader.header = saveHeader(id, sh.header);
+  signedHeader.commit = saveCommit(id, sh.commit);
   signedHeader.save();
+  return id;
 }
 
-function saveValidatorSet(id: string, sh: tendermint.ValidatorSet): void {
-  saveValidator(id, sh.proposer)
-
+function saveValidatorSet(id: string, sh: tendermint.ValidatorSet): string {
   const validatorSet = new ValidatorSet(id);
   validatorSet.validators = saveValidators(id, sh.validators);
-  validatorSet.proposer = id;
+  validatorSet.proposer = saveValidator(id, sh.proposer);
   validatorSet.total_voting_power = BigInt.fromString(sh.total_voting_power.toString());
   validatorSet.save();
+  return id;
 }
 
-function saveValidators(id: string, v: Array<tendermint.Validator>): Array<string> {
-  const len = v.length;
-  let validatorIDs = new Array<string>(len);
-  for (let i = 0; i < len; i++) {
-    const validator = v[i];
-    const validatorID = `${id}-${i}`;
-    saveValidator(validatorID, validator);
-    validatorIDs[i] = validatorID;
-  }
+function saveValidators(id: string, validators: Array<tendermint.Validator>): Array<string> {
+  let validatorIDs = new Array<string>(validators.length);
+  validators.forEach((v, i) => {
+    validatorIDs[i] = saveValidator(`${id}-${i}`, v);
+  });
   return validatorIDs;
 }
 
-function saveValidator(id: string, v: tendermint.Validator): void {
-  savePublicKey(v.address.toHexString(), v.pub_key);
-
+function saveValidator(id: string, v: tendermint.Validator): string {
   const validator = new Validator(id);
   validator.address = v.address;
   validator.voting_power = BigInt.fromString(v.voting_power.toString());
   validator.proposer_priority = BigInt.fromString(v.proposer_priority.toString());
+  validator.pub_key = savePublicKey(v.address.toHexString(), v.pub_key);
   validator.save();
+  return id;
 }
 
-function saveCommit(id: string, c: tendermint.Commit): void {
-  saveBlockID(id, c.block_id);
-
+function saveCommit(id: string, c: tendermint.Commit): string {
   const commit = new Commit(id);
   commit.height = BigInt.fromString(c.height.toString());
   commit.round = c.round;
-  commit.block_id = c.block_id.hash.toHexString();
+  commit.block_id = saveBlockID(c.block_id.hash.toHexString(), c.block_id)
   commit.signatures = saveCommitSigs(id, c.signatures);
   commit.save();
+  return id;
 }
 
 function saveCommitSigs(id: string, cs: Array<tendermint.CommitSig>): Array<string> {
-  const len = cs.length;
-  log.info("signatures len: {}", [len.toString()])
-  let commitSigIDs = new Array<string>(len);
-  for (let i = 0; i < len; i++) {
-    const commitSig = cs[i];
-    const commitSigID = `${id}-${i}`;
-    saveCommitSig(commitSigID, commitSig);
-    commitSigIDs[i] = commitSigID;
-  }
+  let commitSigIDs = new Array<string>(cs.length);
+  cs.forEach((c, i) => {
+    commitSigIDs[i] = saveCommitSig(`${id}-${i}`, c);
+  });
   return commitSigIDs;
 }
 
-function saveCommitSig(id: string, cs: tendermint.CommitSig): void {
-  saveTimestamp(id, cs.timestamp);
-  
+function saveCommitSig(id: string, cs: tendermint.CommitSig): string {
   const commitSig = new CommitSig(id);
   commitSig.block_id_flag = getBlockIDFlag(cs.block_id_flag);
   commitSig.validator_address = cs.validator_address;
-  commitSig.timestamp = id;
+  commitSig.timestamp = saveTimestamp(id, cs.timestamp);
   commitSig.signature = cs.signature;
   commitSig.save();
+  return id;
 }
 
 function getBlockIDFlag(bf: tendermint.BlockIDFlag): string {
@@ -322,39 +298,34 @@ function saveEndBlock(id: string, endBlock: tendermint.ResponseEndBlock): void {
 }
 
 function saveValidatorUpdates(id: string, validators: Array<tendermint.ValidatorUpdate>): Array<string> {
-  const len = validators.length;
-  let validatorIDs = new Array<string>(len);
-  for (let i = 0; i < len; i++) {
-    const validatorUpdate = validators[i];
-    const validatorUpdateID = `${id}-${validatorUpdate.address}`;
-    saveValidatorUpdate(validatorUpdateID, validatorUpdate);
-    validatorIDs[i] = validatorUpdateID;
-  }
+  let validatorIDs = new Array<string>(validators.length);
+  validators.forEach((vs, i) => {
+    validatorIDs[i] = saveValidatorUpdate(`${id}-${vs.address}`, vs);
+  });
   return validatorIDs;
 }
 
-function saveValidatorUpdate(id: string, v: tendermint.ValidatorUpdate): void {
-  const validatorAddress = v.address.toHexString();
-  savePublicKey(validatorAddress, v.pub_key);
-
+function saveValidatorUpdate(id: string, v: tendermint.ValidatorUpdate): string {
   const validatorUpdate = new ValidatorUpdate(id);
   validatorUpdate.address = v.address;
-  validatorUpdate.pub_key = validatorAddress;
+  validatorUpdate.pub_key = savePublicKey(v.address.toHexString(), v.pub_key);;
   validatorUpdate.power = BigInt.fromString(v.power.toString());
   validatorUpdate.save();
+  return id;
 }
 
-function savePublicKey(id: string, publicKey: tendermint.PublicKey): void {
+function savePublicKey(id: string, publicKey: tendermint.PublicKey): string {
   let pk = PublicKey.load(id);
   if (pk !== null) {
     log.debug("Validator with id: {} already exists", [id])
-    return
+    return id;
   }
 
   pk = new PublicKey(id);
   pk.ed25519 = publicKey.ed25519;
   pk.secp256k1 = publicKey.secp256k1;
   pk.save();
+  return id;
 }
 
 export function handleReward(eventData: tendermint.EventData): void {
@@ -362,7 +333,7 @@ export function handleReward(eventData: tendermint.EventData): void {
   const amount = eventData.event.attributes[0].value;
   const validator = eventData.event.attributes[1].value;
 
-  // log.info("REWARD amount = {}, validator = {}", [amount, validator]);
+  log.info("REWARD amount = {}, validator = {}", [amount, validator]);
 
   let reward = new Reward(`${height}-${validator}`);
 
